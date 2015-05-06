6c4a79c6c22fdcf0a907f457b62eb6b51bb67d51
Luis Malheiro <lmalheiro@vivocha.com>
2015-05-06 22:26:48 +0200

    Went back to use mongodb Add possibility to ignore the enqueue result Added queue stats Possibility to requeue stale requests

--- test/poster.js
+++ test/poster.js
@@ -1,5 +1,6 @@
 var k = require('../lib/koda.js')
   , mongodb = require('mongodb')
+  , util = require('util')
 
 mongodb.MongoClient.connect('mongodb://localhost/test', function(err, db) {
   if (err || !db) {
@@ -8,15 +9,28 @@ mongodb.MongoClient.connect('mongodb://localhost/test', function(err, db) {
   }
   
   var koda = k.create(db);
-  koda.on('ready', function() {
-    console.log('koda ready');
-    koda.enqueue('test', { blabla: true }, function(err, data) {
-      console.log('enqueue callback', err, data);
-      process.exit(0);
-    });
-  });
   koda.on('error', function(err) {
     console.error('coda init failed', err, err.stack);
     process.exit(1);
   });
+  //console.log(util.inspect(koda, true, null, true))
+  koda.onReady(function() {
+    console.log('koda ready');
+    koda.enqueue('job_a', { blabla: true }, function(err, data) {
+      console.log('enqueue job_a callback', err, data);
+    });
+    koda.enqueue('job_b', { oops: true }, { expires: 5000 /*milliseconds*/, ignoreResult: true }, function(err, data) {
+      console.log('enqueue job_b callback', err, data);
+    });
+    function print_queue_stats() {
+      koda.stats(function (err, result) {
+        console.log(err, result);
+        setTimeout(print_queue_stats, 5000) ;
+      })
+    }
+
+    print_queue_stats()
+
+  });
+
 });
--- test/worker.js
+++ test/worker.js
@@ -2,17 +2,57 @@ var k = require('../lib/koda.js')
   , mongodb = require('mongodb')
 
 mongodb.MongoClient.connect('mongodb://localhost/test', function(err, db) {
+
   if (err || !db) {
     console.error('failed to connect to db', err, db);
     process.exit(1);
   }
   
   var koda = k.create(db);
-  var handler = function(event) {
-    console.log('got test event', event);
-    setTimeout(function() {
-      koda.next('test', handler);
-    }, 5000)
-  }
-  koda.next('test', handler);
+
+  koda.onReady(function () {
+
+    console.log('ready');
+
+    koda.on('error', function (err) {
+      console.error('queue has problems, check database', err)
+      process.exit(1)
+    })
+
+    // "requeues" jobs that were in assigned state in case of restart due to crashing
+    koda.requeueStale(['job_a', 'job_b'], function (err, result) {
+      
+      console.log('requeueStale');
+
+      function next_job_a() {
+
+        console.log('next_job_a');
+
+        // blocks while new job doesn't arrive
+        koda.next('job_a',function(event, finish) {
+          console.log('got job A event', event);
+          finish(null); // marks jobs as completed (null) or failed (!null)
+          next_job_a(); 
+        }); 
+      }
+
+      function next_job_b() {
+
+        console.log('next_job_b');
+
+        // blocks while new job doesn't arrive
+        koda.next('job_b',function(event, finish) {
+          console.log('got job B event', event);
+          finish(null);
+          next_job_b(); 
+        }); 
+      }
+
+      next_job_a();
+      next_job_b();
+
+    })
+
+  })
+
 });
